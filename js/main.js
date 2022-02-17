081652ee8c66df1e019f384733730449e1e24585
Greisybg <95661929+Greisybg@users.noreply.github.com>
2022-02-16 23:52:33 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,51 +1,73 @@
-let ordenComidaSeleccionada;
-let ordenBebidaSeleccionada;
 
-alert("Bienvenido!\nA continuación te muestro el menú de desayuno disponible, elije una opción");
+const drinkType = "BEBIDA";
+const footType = "COMIDA";
 
-do {
-    let ordenComida = prompt("Ingresa la letra según lo que deseas ordenar:\n S-sandwich \n C-criollos \n M-medialunas \n X-nada");
-    ordenComidaSeleccionada = obtenerOpcionComida(ordenComida)
-} while (ordenComidaSeleccionada == null);
+class ProductoFacturable {
+    constructor(name, description, price, quantity, productype) {
+      this.name = name;
+      this.description = description;
+      this.price = price;
+      this.quantity = quantity;
+      this.productype = productype;
+    }
+  }
+  let sandwich = new ProductoFacturable("Sandwich", "Pan relleno con queso y jamon", 400, 0, footType); 
+  let  tinyBread = new ProductoFacturable("Criollos", "Criollo de hojaldre", 30, 0, footType);  
+  let moonMiddle = new ProductoFacturable("Medialunas", "Croassant dulce", 120, 0, footType);  
+  let coffee = new ProductoFacturable("Cafe con leche", "Cafe tostado con leche", 200, 0, drinkType); 
+  let lemonade = new ProductoFacturable("Limonada", "Bebida fria de limon", 300, 0, drinkType); 
+  let water = new ProductoFacturable("Agua", "Agua mineral", 110, 0, drinkType); 
 
-let deseaBebida = prompt("Desea ordenar algo de bebida? si/no");
+  let products = [sandwich, tinyBread, moonMiddle, coffee, lemonade, water];
 
-if (deseaBebida == "si") {
-    alert("Te muestro el menú de bebidas disponibles, elije una opción");
+alert("Bienvenido!\nA continuación te muestro el menú de desayuno");
+getOrder(footType);
+getOrder(drinkType);
+getBill();
+function verificarOpcion(opcion) {
+    esValida = (!isNaN(opcion) && parseInt(opcion) >= 0 && parseInt(opcion) < products.length) ;
+    if (esValida) {
+        return  parseInt(opcion);
+    } else {
+        return null;
+    }
+}
+
+function getProducts(productType){
+    let productslist = ""; 
+    for (i = 0; i < products.length; i++) {
+        if (products[i].productype==productType){
+            productslist =productslist+ "Cod: "+ i + " Nombre: " + products[i].name + "\n";
+        }
+      }
+      return productslist;
+ }
+
+ function getOrder(productType){
+    let selectedCode = -1;
     do {
-        ordenBebida = prompt("Ingresa la letra según lo que deseas ordenar:\n C-cafe con leche \n L-limonada \n A-agua \n X-nada");
-        ordenBebidaSeleccionada = obtenerOpcionBebida(ordenBebida)
-    } while (ordenBebidaSeleccionada == null);
-} else {
-    ordenBebidaSeleccionada = "X"
-}
+        selectedCode = prompt("LISTA \n" + getProducts(productType)+"Ingresa el codigo del producto según lo que deseas ordenar: ");
+    } while (verificarOpcion(selectedCode) == null);
+    setQuantityProduc(selectedCode);
+ }
 
-if (ordenComidaSeleccionada != "X" && ordenBebidaSeleccionada != "X") {
-    alert("Gracias por su compra!\nA continuación le muestro el total a pagar por su pedido");
-} else if (ordenComidaSeleccionada != "X") {
-    alert("Usted solo ha pedido una comida!\nA continuación le muestro el total a pagar por su pedido");
-} else if (ordenBebidaSeleccionada != "X") {
-    alert("Usted solo ha pedido una bebida!\nA continuación le muestro el total a pagar por su pedido");
-} else {
-    alert("No ha seleccionado ningun producto para comprar \n Hasta luego!");
-}
+ function setQuantityProduc(selectedCode){
+    let quantity
+    do {
+        quantity = prompt("Usted ha seleccionado \n" + "Nombre: " + products[selectedCode].name + "\n Descripción: " + products[selectedCode].description + "\n Precio: " + products[selectedCode].price +"\n Ingresa el codigo del producto según lo que deseas ordenar: ");
+    } while (isNaN(quantity));
+    products[selectedCode].quantity = parseInt(quantity);
+ }
 
-function obtenerOpcionComida(opcion) {
-    let opcionInterna = opcion.trim().toUpperCase()
-    let esValida = opcionInterna == "S" || opcionInterna == "C" || opcionInterna == "M" || opcionInterna == "X";
-    if (esValida) {
-        return opcionInterna
-    } else {
-        return null;
-    }
-}
-
-function obtenerOpcionBebida(opcion) {
-    let opcionInterna = opcion.trim().toUpperCase()
-    let esValida = opcionInterna == "C" || opcionInterna == "L" || opcionInterna == "A" || opcionInterna == "X";
-    if (esValida) {
-        return opcionInterna
-    } else {
-        return null;
-    }
-}
+ function getBill(){
+    let productslist = "Detalles de facturación \n"; 
+    let total = 0;
+    for (i = 0; i < products.length; i++) {
+        if (products[i].quantity > 0){
+            productslist =productslist+ "Cod: "+ i + " Nombre: " + products[i].name + " Cantidad: "+products[i].quantity+" Subtotal: " + products[i].quantity * products[i].price+ "\n";
+            total = total +( products[i].quantity * products[i].price);
+        }
+      }
+      productslist = productslist + "TOTAL: " + total;
+      alert( productslist);
+ }
